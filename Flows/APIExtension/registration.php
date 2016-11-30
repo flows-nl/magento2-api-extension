e34cda5cfc3e403961bef450397752e2489c25f3
Gert Wijnalda <gert@redant.nl>
2016-11-30 14:25:35 +0100

    Initial commit

---
+++ Flows/APIExtension/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Flows
+ * @copyright Copyright (c) 2016 Flows (http://flows.net)
+ * @package Flows_APIExtension
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Flows_APIExtension',
+    __DIR__
+);
+
