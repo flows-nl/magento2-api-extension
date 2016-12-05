bde56da713df2c426fac9aa6c89585bd7d9a5705
Gert Wijnalda <gert@redant.nl>
2016-12-05 11:25:35 +0100

    Restructured module folder layout

--- registration.php
+++ registration.php
@@ -1,12 +1,14 @@
 <?php
 /**
- * @author Flows_APIExtension
- * @copyright Copyright (c) 2016 Flows_APIExtension (http://flows.net)
+ * @author Flows
+ * @copyright Copyright (c) 2016 Flows (http://flows.net)
+ * @author Gert Wijnalda <gert@redant.nl>
+ * @author Vincent Vermeulen <vincent@redant.nl>
  * @package Flows_APIExtension
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Flows_APIExtension',
-    __DIR__ . DIRECTORY_SEPARATOR . 'Flows_APIExtension'
+    __DIR__
 );
 
